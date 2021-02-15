f6474c0548ef456fb6b6a52f82866cd80d1fb456
Anders Holt Knudsen <andhk11@gmail.com>
2021-02-15 22:06:46 +0100

    Added Blog Posts from sanity

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React from "react";
+
+// <img src="" alt="" className="absolute object-cover w-full h-full" />
 
 const Home = () => {
   return (
-    <div>
-      <h1>Ya, mama!</h1>
-    </div>
+    <main>
+      <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+        <h1 className="text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name">
+          Hallo, I'm Anders.
+        </h1>
+      </section>
+    </main>
   );
 };
 
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,10 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import sanityClient from "../client";
 
 const Project = () => {
-  return (
-    <div>
+  const [projectData, setProjectData] = useState(null);
 
-    </div>
+  useEffect(() => {
+    sanityClient
+      .fetch(
+        `*[_type == "project"]{
+      title,
+      date,
+      place,
+      description,
+      projectType,
+      link,
+      tags`
+      )
+      .then((data) => setProjectData(data))
+      .catch(console.error);
+  }, []);
+
+  return (
+    <main>
+      <section>
+        <h1></h1>
+        <h2></h2>
+        <section>
+          <article>
+            <h3></h3>
+            <div>
+              <span></span>
+              <span></span>
+              <span></span>
+              <p></p>
+              <span></span>
+            </div>
+          </article>
+        </section>
+      </section>
+    </main>
   );
 };
 
