abc960c2b11756eee3691377b50d51328e617637
Anders Holt Knudsen <andhk11@gmail.com>
2021-02-17 22:43:10 +0100

    Finished tutorial from YouTube

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -14,28 +14,63 @@ const Project = () => {
       description,
       projectType,
       link,
-      tags`
+      tags}`
       )
       .then((data) => setProjectData(data))
       .catch(console.error);
   }, []);
 
+  console.log(projectData);
+
   return (
-    <main>
-      <section>
-        <h1></h1>
-        <h2></h2>
-        <section>
-          <article>
-            <h3></h3>
-            <div>
-              <span></span>
-              <span></span>
-              <span></span>
-              <p></p>
-              <span></span>
-            </div>
-          </article>
+    <main className="bg-green-100 min-h-screen p-12">
+      <section className="container mx-auto">
+        <h1 className="text-5xæ flex justify-center cursive">My Projects</h1>
+        <h2 className="text-lg text-gray-600 flex justify-center mb-12">
+          This is all the projects
+        </h2>
+        <section className="grid grid-cols-2 gap-8">
+          {projectData &&
+            projectData.map((project) => (
+              <article className="relative rounded-lg shadow-xl bg-white p-16">
+                <h3 className="text-gray-800 text-3xl font-bold mb-2 hover:text-red-7000">
+                  <a
+                    href={project.link}
+                    alt={project.title}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {project.title}
+                  </a>
+                </h3>
+                <div className="text-gray-500 text-xs space-x-4">
+                  <span>
+                    <strong className="font-bold">Finished on</strong>:{" "}
+                    {new Date(project.date).toLocaleDateString()}
+                  </span>
+                  <span>
+                    <strong className="font-bold">Company</strong>:{" "}
+                    {project.place}
+                  </span>
+                  <span>
+                    <strong className="font-bold">Type</strong>:{" "}
+                    {project.projectType}
+                  </span>
+                  <p className="my-6 text-lg text-gray-700 leading-relaxed">
+                    {project.description}
+                  </p>
+                  <a
+                    className="text-red-500 font-bold hover:underline hover:text-red-400"
+                    href={project.link}
+                    alt={project.title}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    View The Project{" "}
+                  </a>
+                </div>
+              </article>
+            ))}
         </section>
       </section>
     </main>
